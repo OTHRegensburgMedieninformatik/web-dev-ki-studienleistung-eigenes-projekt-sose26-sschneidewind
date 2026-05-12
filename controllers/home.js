6f1d1fd983d16b1c4c377f08e1961b7604d136a9
Angry-Pigeon-cmd <soerenschneidewind@gmail.com>
2026-05-12 23:41:59 +0200

    Added the following features: - display of best dishes / restaurants on the homepage - added logic to switch between global best restaurants / dishes when not logged in and local best restaurants / dishes when logged in

--- controllers/home.js
+++ controllers/home.js
@@ -1,13 +1,58 @@
 const logger = require("../utils/logger.js");
 
+function get_global_restaurants() {
+  //just get best global restaurants
+  return [
+    {place: 1, restaurant: "Luigis Pizzeria", rest_id:1},
+    {place: 2, restaurant: "Marios Nudel Restaurant", rest_id:2},
+    {place: 3, restaurant: "Gumbat Rakete", rest_id:3}
+  ]
+}
+
+function get_local_restaurants(email)  {
+  //here will be a lookup for local restaurants near user with email
+  return [
+    {place: 1, restaurant: "Gumbat Rakete", rest_id:3},
+    {place: 2, restaurant: "Marios Nudel Restaurant", rest_id:2},
+    {place: 3, restaurant: "Luigis Pizzeria", rest_id:1}
+  ]
+}
+
+function get_global_dishes() {
+  //just get globally best performing dishes
+  return [
+    {place: 1, restaurant: "Luigis Pizzeria", rest_id:1, dish: "Pizza Margherita", dish_id : 1},
+    {place: 2, restaurant: "Gumbat Rakete", rest_id:3, dish: "soup soup soup", dish_id : 1},
+    {place: 3, restaurant: "Marios Nudel Restaurant", rest_id:2, dish: "Nudelsuppe rot weiß", dish_id : 1},
+  ]
+}
+
+function get_local_dishes(email) {
+  //here will be another lookup but this time for the best dish of all restaurants near one
+  return [
+    {place: 1, restaurant: "Gumbat Rakete", rest_id:3, dish: "soup soup soup", dish_id : 1},
+    {place: 2, restaurant: "Marios Nudel Restaurant", rest_id:2, dish: "Nudelsuppe rot weiß", dish_id : 1},
+    {place: 3, restaurant: "Luigis Pizzeria", rest_id:1, dish: "Pizza Margherita", dish_id : 1}
+  ]
+}
+
 const home = {
   index(request, response) {
     logger.info("home rendering, name is: " + request.session.name);
+    if (request.session.signed_in) {
+      restaurants = get_local_restaurants(request.session.email);
+      dishes = get_local_dishes(request.session.email);
+    } else {
+      restaurants = get_global_restaurants();
+      dishes = get_global_dishes();
+    }
     const viewData = {
       title: "Critical Restaurant",
       signed_in: request.session.signed_in,
       name: request.session.name,
-      surname: request.session.surname
+      surname: request.session.surname,
+      restaurants: restaurants,
+      dishes: dishes
     };
     response.render("home", viewData);
   },
